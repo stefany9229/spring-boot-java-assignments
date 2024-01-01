5f1ade258125056faac1fee5252dfd3aff2b42b9
StefanySB <stefany9209@gmail.com>
2024-01-01 16:01:47 -0500

    Se pasan test de JWT

--- src/main/java/org/adaschool/api/security/JwtRequestFilter.java
+++ src/main/java/org/adaschool/api/security/JwtRequestFilter.java
@@ -1,13 +1,22 @@
 package org.adaschool.api.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.adaschool.api.controller.auth.TokenAuthentication;
+import org.adaschool.api.exception.TokenExpiredException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 import java.io.IOException;
+import java.util.List;
+
+import static org.adaschool.api.utils.Constants.CLAIMS_ROLES_KEY;
 
 
 @Component
@@ -21,7 +30,21 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //TODO implementar este Metodo
+        final String authorizationHeader = request.getHeader("Authorization");
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String jwt = authorizationHeader.substring(7);
+                Claims claims = jwtUtil.extractAndVerifyClaims(jwt);
+                String username = claims.getSubject();
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    List<String> roles = claims.get(CLAIMS_ROLES_KEY, List.class);
+                    TokenAuthentication tokenAuthentication = new TokenAuthentication(jwt, username, roles);
+                    SecurityContextHolder.getContext().setAuthentication(tokenAuthentication);
+                }
+            }
+        } catch (ExpiredJwtException e) {
+            throw new TokenExpiredException();
+        }
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/org/adaschool/api/security/SecurityConfiguration.java
+++ src/main/java/org/adaschool/api/security/SecurityConfiguration.java
@@ -6,9 +6,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
@@ -39,7 +44,17 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //TODO implementar este Metodo
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/v1/health", "/api/v1/auth").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
         return http.build();
     }
 
